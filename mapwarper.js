9dde22dfc05f6d0daee77f89a6283650d2528753
Bert Spaan <bert.spaan@gmail.com>
2017-09-27 09:39:51 -0400

    download map layers

--- mapwarper.js
+++ mapwarper.js
@@ -15,50 +15,87 @@ const GOT_OPTIONS = {
   json: true
 }
 
-const getUrl = (perPage, page) => `http://maps.nypl.org/warper/maps.json?per_page=${perPage}&page=${page}`
+const SLEEP_MS = 2000
 
-function requestCallback (sleep, url, callback) {
-  console.log('\tDownloading ' + url + ' (and sleeping ' + sleep + 'ms)')
+const BASE_URL = 'http://maps.nypl.org/warper/'
+const PER_PAGE = 250
 
-  got(url, GOT_OPTIONS)
+const paginate = (page = 1, perPage = PER_PAGE) => `per_page=${perPage}${page > 1 ? `&page=${page}` : ''}`
+const getMapsUrl = (page, perPage) => `${BASE_URL}maps.json?${paginate(page, perPage)}`
+const getLayersUrl = (page, perPage) => `${BASE_URL}layers.json?${paginate(page, perPage)}`
+const getMapLayersUrl = (mapId, page, perPage) => `${BASE_URL}maps/${mapId}/layers.json?${paginate(page, perPage)}`
+
+function downloadUrl (url, sleep) {
+  console.log('\tDownloading ' + url + (sleep ? ' (and sleeping ' + sleep + 'ms)' : ''))
+
+  return got(url, GOT_OPTIONS)
     .then((response) => {
       if (sleep) {
-        setTimeout(() => {
-          callback(null, response.body)
-        }, sleep)
+        return new Promise((resolve, reject) => {
+          setTimeout(() => resolve(response.body), sleep)
+        })
       } else {
-        callback(null, response.body)
+        return response.body
       }
+    })
+}
+
+function downloadUrlCallback (sleep, url, callback) {
+  downloadUrl(url, sleep)
+    .then((body) => {
+      callback(null, body)
     })
     .catch(callback)
 }
 
-function getUrls (perPage, items) {
-  const count = Math.ceil(items / perPage)
-
-  let urls = []
-  for (var page = 1; page <= count; page++) {
-    urls.push(getUrl(perPage, page))
-  }
+function getMapsUrls (totalItems) {
+  const count = Math.ceil(totalItems / PER_PAGE)
+  const urls = Array.from({length: count})
+    .map((d, index) => getMapsUrl(index + 1))
 
   return urls
 }
 
+function getMapLayers (map, callback) {
+  downloadLayers(map.id, SLEEP_MS / 10)
+    .then((results) => {
+      const layerIds = results
+        .filter((result) => result.type === 'layer')
+        .map((result) => result.layer.id)
+
+      const layerErrors = results
+        .filter((result) => result.type === 'error')
+        .map((result) => ({
+          error: result.error,
+          url: result.url
+        }))
+
+      callback(null, Object.assign(map, {
+        layerIds,
+        layerErrors
+      }))
+    })
+    .catch(callback)
+}
+
 function getMask (sleep, map, callback) {
-  var maskStatus = map.mask_status
+  const maskStatus = map.mask_status
   if (maskStatus === 'masked' || maskStatus === 'masking') {
     console.log(`          Getting mask for map ${map.id}`)
 
     maskToGeoJSON.getMaskAndTransform({
       mapId: map.id,
       transform: map.transform_options
-    }, (err, geojson) => {
+    }, (err, geojson, gcps, mask) => {
       if (err) {
         console.error(err.message)
         map.maskError = err.message
       } else {
         console.log(`          Transformed mask for map ${map.id}: ${geojson.coordinates[0].length} points`)
-        map.mask = geojson
+
+        map.mask = mask
+        map.gcps = gcps
+        map.maskGeometry = geojson
       }
 
       if (sleep) {
@@ -74,45 +111,105 @@ function getMask (sleep, map, callback) {
   }
 }
 
-function download (config, dirs, tools, callback) {
-  const sleepMs = 2000
-  const perPage = 250
+async function downloadLayers (mapId, sleep) {
+  let page = 1
+  const results = []
 
-  maskToGeoJSON.gdalInstalled((err) => {
-    if (err) {
-      callback(new Error('GDAL is not installed - GDAL is needed to convert Map Warper masks to GeoJSON'))
-    } else {
-      got(getUrl(1, 1), GOT_OPTIONS)
-        .then((response) => {
-          H([response.body.total_entries])
-            .map(H.curry(getUrls, perPage))
-            .flatten()
-            .map(H.curry(requestCallback, sleepMs))
-            .nfcall([])
-            .series()
-            .map((body) => body.items)
-            .flatten()
-            .compact()
-            .map(H.curry(getMask, sleepMs / 20))
-            .nfcall([])
-            .series()
-            .errors(callback)
-            .map(JSON.stringify)
-            .intersperse('\n')
-            .pipe(fs.createWriteStream(path.join(dirs.current, 'maps.ndjson')))
-            .on('finish', callback)
-        })
-        .catch(callback)
+  while (true) {
+    const url = mapId ? getMapLayersUrl(mapId, page) : getLayersUrl(page)
+
+    let result
+    try {
+      result = await downloadUrl(url, sleep)
+    } catch (err) {
+      results.push({
+        type: 'error',
+        mapId,
+        page,
+        url,
+        error: err.message
+      })
     }
-  })
+
+    if (result && result.items && result.items.length) {
+      results.push(...result.items.map((result) => ({
+        type: 'layer',
+        layer: result
+      })))
+
+      if (result.items.length < PER_PAGE) {
+        break
+      }
+    } else {
+      break
+    }
+
+    page += 1
+  }
+
+  return results
 }
 
-const checkMap = (map) => Object.assign(map, {logs: getLogs(map)})
+function download (config, dirs, tools, callback) {
+  const mapsWriteStream = fs.createWriteStream(path.join(dirs.current, 'maps.ndjson'))
+  const layersWriteStream = fs.createWriteStream(path.join(dirs.current, 'layers.ndjson'))
+
+  downloadLayers()
+    .then((results) => {
+      H(results)
+        .flatten()
+        .filter((result) => result.type === 'layer')
+        .map((result) => ({
+          type: 'layer',
+          data: result.layer
+        }))
+        .map(JSON.stringify)
+        .intersperse('\n')
+        .pipe(layersWriteStream)
+
+      maskToGeoJSON.gdalInstalled((err) => {
+        if (err) {
+          callback(new Error('GDAL is not installed - GDAL is needed to convert Map Warper masks to GeoJSON'))
+        } else {
+          downloadUrl(getMapsUrl())
+            .then((body) => {
+              H([body.total_entries])
+                .map(H.curry(getMapsUrls))
+                .flatten()
+                .map(H.curry(downloadUrlCallback, SLEEP_MS))
+                .nfcall([])
+                .series()
+                .map((body) => body.items)
+                .flatten()
+                .compact()
+                .map(H.curry(getMapLayers))
+                .nfcall([])
+                .series()
+                .errors(callback)
+                .map(H.curry(getMask, SLEEP_MS / 20))
+                .nfcall([])
+                .series()
+                .errors(callback)
+                .map((map) => ({
+                  type: 'map',
+                  data: map
+                }))
+                .map(JSON.stringify)
+                .intersperse('\n')
+                .pipe(mapsWriteStream)
+                .on('finish', callback)
+            })
+            .catch(callback)
+        }
+      })
+    })
+    .catch(callback)
+}
 
 function getLogs (map) {
-  var log = {
+  const log = {
     id: map.id,
-    nyplDigitalId: map.nypl_digital_id,
+    imageId: map.nypl_digital_id,
     logs: []
   }
 
@@ -120,17 +217,17 @@ function getLogs (map) {
   const maskStatus = map.mask_status
 
   // Check mask's number of coordinates:
-  var minCoordinatesCount = 4
-  if (map.mask && map.mask.coordinates[0].length < minCoordinatesCount) {
+  const minCoordinatesCount = 4
+  if (map.maskGeometry && map.maskGeometry.coordinates[0].length < minCoordinatesCount) {
     log.logs.push({
       type: 'mask_coordinates_count',
-      message: `Mask has ${map.mask.coordinates[0].length} coordinates (should have at least ${minCoordinatesCount})`
+      message: `Mask has ${map.maskGeometry.coordinates[0].length} coordinates (should have at least ${minCoordinatesCount})`
     })
   }
 
   // Check if mask has self-intersections:
-  if (map.mask) {
-    const kinks = turf.kinks(map.mask)
+  if (map.maskGeometry) {
+    const kinks = turf.kinks(map.maskGeometry)
     if (kinks.features.length) {
       log.logs.push({
         type: 'self_intersection',
@@ -140,7 +237,7 @@ function getLogs (map) {
   }
 
   // Check if mask's coordinates are valid:
-  if (map.mask) {
+  if (map.maskGeometry) {
     const coordValid = (coord) => {
       const lat = coord[1]
       const lon = coord[0]
@@ -148,7 +245,7 @@ function getLogs (map) {
       return lon >= -180 && lon <= 180 && lat >= -90 && lat <= 90
     }
 
-    const allValid = turf.meta.coordAll(map.mask)
+    const allValid = turf.meta.coordAll(map.maskGeometry)
       .reduce((a, b) => a && coordValid(b), true)
 
     if (!allValid) {
@@ -160,10 +257,10 @@ function getLogs (map) {
   }
 
   // Check if mask is a MultiPolygon:
-  if (map.mask && map.mask.coordinates.length !== 1) {
+  if (map.maskGeometry && map.maskGeometry.coordinates.length !== 1) {
     log.logs.push({
       type: 'multipolygon',
-      message: `Mask is a MultiPolygon with ${map.mask.coordinates.length} polygons`
+      message: `Mask is a MultiPolygon with ${map.maskGeometry.coordinates.length} polygons`
     })
   }
 
@@ -188,7 +285,7 @@ function getLogs (map) {
     })
   }
 
-  if (log.logs.length === 0 && !(map.mask && map.mask.coordinates)) {
+  if (log.logs.length === 0 && !(map.maskGeometry && map.maskGeometry.coordinates)) {
     log.logs.push({
       type: 'mask_missing',
       message: 'Map is unmasked'
@@ -200,55 +297,115 @@ function getLogs (map) {
       type: 'log',
       obj: log
     }
+  }
+}
+
+function getYear (object) {
+  const year = object.depicts_year || object.issue_year
+
+  if (year) {
+    return parseInt(year)
+  }
+}
+
+const getLayerId = (layerId) => `layer-${layerId}`
+
+function getLayerRelations (map) {
+  return map.layerIds.map((layerId) => ({
+    type: 'relation',
+    obj: {
+      type: 'st:in',
+      from: map.id,
+      to: getLayerId(layerId)
+    }
+  }))
+}
+
+function transformMap (map) {
+  if (!map.bbox || map.map_type !== 'is_map') {
+    return
+  }
+
+  const logs = getLogs(map)
+  if (logs) {
+    // Something's not right! Only write logs, do not write map!
+    return logs
   } else {
-    return null
+    const geometry = map.maskGeometry
+    const area = Math.round(turf.area(geometry))
+
+    // console.log(map.id)
+    if (String(map.uuid).startsWith('inset')) {
+      console.log('INSET INSET INSET INSET INSET INSET INSET INSET INSET ')
+      console.log(map)
+
+      // http://api.repo.nypl.org/api/v1/items/local_image_id/2001548
+    }
+
+    // console.log(map.id, map.id.startsWith('inset'))
+
+    const object = {
+      type: 'object',
+      obj: {
+        id: map.id,
+        type: 'st:Map',
+        name: map.title,
+        validSince: getYear(map),
+        validUntil: getYear(map),
+        data: {
+          description: map.description,
+          imageId: map.nypl_digital_id,
+          uuid: map.uuid,
+          parentUuid: map.parent_uuid,
+          masked: map.mask_status === 'masked' || map.mask_status === 'masking',
+          nyplUrl: `http://digitalcollections.nypl.org/items/${map.uuid}`,
+          tileUrl: `http://maps.nypl.org/warper/maps/tile/${map.id}/{z}/{x}/{y}.png`,
+          area: area * 0.000001,
+          gcps: map.gcps
+        },
+        geometry
+      }
+    }
+
+    return [
+      object,
+      ...getLayerRelations(map)
+    ]
+  }
+}
+
+function transformLayer (layer) {
+  return {
+    type: 'object',
+    obj: {
+      id: getLayerId(layer.id),
+      type: 'st:Map',
+      name: layer.name,
+      validSince: getYear(layer),
+      validUntil: getYear(layer),
+      data: {
+        mapCount: layer.maps_count,
+        tileUrl: `http://maps.nypl.org/warper/layers/tile/${layer.id}/{z}/{x}/{y}.png`,
+        bbox: layer.bbox && layer.bbox.split(',').map(parseFloat)
+      }
+    }
   }
 }
 
 function transform (config, dirs, tools, callback) {
-  H(fs.createReadStream(path.join(dirs.previous, 'maps.ndjson')))
+  const transforms = {
+    map: transformMap,
+    layer: transformLayer
+  }
+
+  H(['maps.ndjson', 'layers.ndjson'])
+    .map((file) => H(fs.createReadStream(path.join(dirs.previous, file))).append('\n'))
+    .sequence()
     .split()
     .compact()
     .map(JSON.parse)
-    .filter((map) => map.bbox)
-    .filter((map) => map.map_type === 'is_map')
-    .map(checkMap)
+    .map((line) => transforms[line.type] && transforms[line.type](line.data))
     .compact()
-    .map((map) => {
-      if (map.logs) {
-        // Something's not right! Only write logs, do not write map!
-        return map.logs
-      } else {
-        const geometry = map.mask
-        const area = Math.round(turf.area(geometry))
-
-        var object = {
-          id: map.id,
-          type: 'st:Map',
-          name: map.title,
-          data: {
-            description: map.description,
-            nyplDigitalId: map.nypl_digital_id,
-            uuid: map.uuid,
-            parentUuid: map.parent_uuid,
-            masked: map.mask_status === 'masked' || map.mask_status === 'masking',
-            nyplUrl: `http://digitalcollections.nypl.org/items/${map.uuid}`,
-            area: area * 0.000001
-          },
-          geometry: geometry
-        }
-
-        if (map.depicts_year || map.issue_year) {
-          object.validSince = map.depicts_year || map.issue_year
-          object.validUntil = map.depicts_year || map.issue_year
-        }
-
-        return {
-          type: 'object',
-          obj: object
-        }
-      }
-    })
     .flatten()
     .map(H.curry(tools.writer.writeObject))
     .nfcall([])
